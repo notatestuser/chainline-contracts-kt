589ac0310c39b207a4ffcd795aed0a786dfa235b
Luke Plaster <me@lukep.org>
2017-10-02 12:11:33 +0800

    Unit tests now dump the evaluation stack on VM fault

--- unit-tests/Utilities/VMHelper.cs
+++ unit-tests/Utilities/VMHelper.cs
@@ -1,4 +1,5 @@
-﻿using Neo.VM;
+﻿using System;
+using Neo.VM;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -6,10 +7,19 @@ namespace CLTests.Utilities {
    public class VMHelper {
       public static void AssertNoFaultState(ExecutionEngine engine, ITestOutputHelper output) {
          bool hasFaulted = engine.State == VMState.FAULT;
+         if (!hasFaulted) return;
+
          output.WriteLine("VM Fault! OpCode: {0}\n{1}\n{2}",
                           ExecutionEngine.LastOpCode.ToString(),
                           ExecutionEngine.LastException.Message,
                           ExecutionEngine.LastException.StackTrace);
+
+         output.WriteLine("Dumping evaluation stack:");
+
+         foreach (StackItem item in ExecutionEngine.LastEvaluationStack) {
+            output.WriteLine(BitConverter.ToString(item.GetByteArray()));
+         }
+
          Assert.False(hasFaulted, "FAULT");
       }
    }

55487106697b5e2823108aa646c55705e195dd7f
Luke Plaster <me@lukep.org>
2017-10-14 13:17:14 +0800

    VMHelper: edit for partial exception info

--- unit-tests/Utilities/VMHelper.cs
+++ unit-tests/Utilities/VMHelper.cs
@@ -9,13 +9,17 @@ namespace CLTests.Utilities {
          bool hasFaulted = engine.State == VMState.FAULT;
          if (!hasFaulted) return;
 
-         output.WriteLine("VM Fault! OpCode: {0}\n{1}\n{2}",
-                          ExecutionEngine.LastOpCode.ToString(),
-                          ExecutionEngine.LastException.Message,
-                          ExecutionEngine.LastException.StackTrace);
+         if (ExecutionEngine.LastException == null) {
+            output.WriteLine("VM Fault! OpCode: {0}",
+                             ExecutionEngine.LastOpCode.ToString());
+         } else {
+            output.WriteLine("VM Fault! OpCode: {0}\n{1}\n{2}",
+                             ExecutionEngine.LastOpCode.ToString(),
+                             ExecutionEngine.LastException.Message,
+                             ExecutionEngine.LastException.StackTrace);
+         }
 
          output.WriteLine("Dumping evaluation stack:");
-
          foreach (StackItem item in ExecutionEngine.LastEvaluationStack) {
             output.WriteLine(BitConverter.ToString(item.GetByteArray()));
          }
